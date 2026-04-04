41da00b9e0963300b78b858290f663a187937070
Daniel Scott <daniel.scott@generalassemb.ly>
2026-04-04 09:45:04 -0500

    wip: hides additional pricing info for now

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -130,7 +130,7 @@ const Pricing = () => {
         </div>
 
         {/* Additional pricing info */}
-        <div className="mt-16 bg-gray-50 rounded-2xl p-8">
+        {/* <div className="mt-16 bg-gray-50 rounded-2xl p-8">
           <div className="max-w-3xl mx-auto text-center">
             <h3 className="text-2xl font-bold text-gray-900 mb-6">
               Pricing Information
@@ -163,7 +163,7 @@ const Pricing = () => {
               </p>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </section>
   );
